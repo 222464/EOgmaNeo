c06b1277c0d80e421429253b8ebcb3a50fcdf20d
Eric Laukien <ericlaukien@gmail.com>
2018-09-27 21:38:45 -0400

    Removed biases

--- source/eogmaneo/Layer.h
+++ source/eogmaneo/Layer.h
@@ -119,8 +119,6 @@ namespace eogmaneo {
 
         std::vector<int> _hiddenStates;
         std::vector<int> _hiddenStatesPrev;
-        
-        std::vector<float> _hiddenBiases;
 
         std::vector<std::vector<std::vector<float>>> _feedForwardWeights;
         std::vector<std::vector<std::vector<float>>> _feedBackWeights;
@@ -171,11 +169,6 @@ namespace eogmaneo {
         float _gamma;
 
         /*!
-        \brief Bias decay factor.
-        */
-        float _delta;
-
-        /*!
         \brief Exploration rate.
         */
         float _epsilon;
@@ -189,7 +182,7 @@ namespace eogmaneo {
         \brief Initialize defaults.
         */
         Layer()
-        : _alpha(0.1f), _beta(0.02f), _gamma(0.98f), _delta(0.0001f), _epsilon(0.01f), _valueHorizon(32)
+        : _alpha(0.001f), _beta(0.02f), _gamma(0.98f), _epsilon(0.01f), _valueHorizon(32)
         {}
 
         /*!
