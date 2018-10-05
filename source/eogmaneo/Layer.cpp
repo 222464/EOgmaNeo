ebd1d9b7647d71f239f713c69af322f35a6986a6
Eric Laukien <ericlaukien@gmail.com>
2018-10-05 14:00:08 -0400

    Added missing params

--- source/eogmaneo/Layer.cpp
+++ source/eogmaneo/Layer.cpp
@@ -448,6 +448,7 @@ void Layer::readFromStream(std::istream &is) {
     is.read(reinterpret_cast<char*>(&_alpha), sizeof(float));
     is.read(reinterpret_cast<char*>(&_beta), sizeof(float));
     is.read(reinterpret_cast<char*>(&_gamma), sizeof(float));
+    is.read(reinterpret_cast<char*>(&_k), sizeof(float));
     is.read(reinterpret_cast<char*>(&_epsilon), sizeof(float));
     is.read(reinterpret_cast<char*>(&_codeIters), sizeof(int));
 
@@ -543,6 +544,7 @@ void Layer::writeToStream(std::ostream &os) {
     os.write(reinterpret_cast<char*>(&_alpha), sizeof(float));
     os.write(reinterpret_cast<char*>(&_beta), sizeof(float));
     os.write(reinterpret_cast<char*>(&_gamma), sizeof(float));
+    os.write(reinterpret_cast<char*>(&_k), sizeof(float));
     os.write(reinterpret_cast<char*>(&_epsilon), sizeof(float));
     os.write(reinterpret_cast<char*>(&_codeIters), sizeof(int));
 
